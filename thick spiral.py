77327e6cdca41ea75816933a25ff90291fde2a09
kanak agarwal <122511074+Kanak-0202@users.noreply.github.com>
2023-06-23 12:21:42 +0530

    Add files via upload

---
+++ thick spiral.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import turtle as t
+colors=["red", "blue", "yellow", "green", "purple", "orange"]
+#t.reset()
+t.tracer(0,0)
+for i in range(45):
+    t.color(colors[i%6])
+    t.pendown()
+    t.forward(2+i*5)
+    t.left(45)
+    t.width(i)
+    t.penup()
+    t.update()
+
